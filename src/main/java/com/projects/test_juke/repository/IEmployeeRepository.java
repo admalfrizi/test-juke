2a169bdfa2f3dd6312151ee37a68b8f814a39791
adamalfarizi <adam.alfarizi.2002@gmail.com>
2025-10-28 11:35:40 +0700

    add comment in IEmployee repository

--- src/main/java/com/projects/test_juke/repository/IEmployeeRepository.java
+++ src/main/java/com/projects/test_juke/repository/IEmployeeRepository.java
@@ -3,5 +3,10 @@ package com.projects.test_juke.repository;
 import com.projects.test_juke.model.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+    This repository is requiring from their own JpaRepository in purpose to connect with application.properties that
+    initialize for JPA Driver for PostgreSQL, so the code function is generated built from JpaRepository
+    and Use in this IEmployeeRepository
+*/
 public interface IEmployeeRepository extends JpaRepository<Employee,Long> {
 }
